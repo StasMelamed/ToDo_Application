dd8d31092d70882b18956d86fd83c3281d1de965
tanislav elamed <stanislavmelamed@gmail.com>
2023-11-15 14:50:55 +0300

    Initial commit

---
+++ @reduxjs/toolkit/src/store/store.ts
@@ -0,0 +1,19 @@
+import {configureStore} from "@reduxjs/toolkit";
+import {TypedUseSelectorHook, useDispatch, useSelector} from "react-redux";
+import user from "../utils/userSlice";
+
+export const store = configureStore({
+        reducer:{
+                user
+        }
+})
+
+
+export type RootState = ReturnType<typeof store.getState>;
+
+type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+
+
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
